931d20e0f1d35cd255a1e196196725abfb31b3d1
H.M.MohaiManul Islam <h.m.mohaimanulislam@HMMohaiManuls-MacBook-Pro.local>
2020-07-15 17:29:42 +0600

    login fixed

--- Router/auth.js
+++ Router/auth.js
@@ -7,8 +7,13 @@ const bcrypt = require('bcrypt');
 router.post('/', async (req, res) => {
   let user = await User.findOne(
     { email: req.body.email },
-    { email: 1, password: 1 }
+    { email: 1, password: 1, isEmailConfirmed: 1 }
   );
+  if (user && user.isEmailConfirmed === false) {
+    return res
+      .status(400)
+      .json('Email is not varified, Please verify your email first');
+  }
   if (!user) return res.status(400).json('invalid email or password');
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).json('invalid email or password');
